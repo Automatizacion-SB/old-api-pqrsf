9846f142b13f4d0dfb413ce68d8f399d1472b319
david g <galvan.dsierra@gmail.com>
2023-09-13 17:51:47 -0500

    feat: :package: add @hapi/boom y manejo de errores

--- app.js
+++ app.js
@@ -2,11 +2,21 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const {
+  logErrors,
+  boomErrorHandler,
+  errorHandler,
+} = require('./middlewares/error.handler');
+
 const PORT = 3000;
 
 const app = express();
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`🚀 Listen in port ${PORT}`);
