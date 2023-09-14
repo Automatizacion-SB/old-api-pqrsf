98c5e89fd046b3e8cfbe03aab64a485e29e60d5f
DesarrolloHisa <soporte@hisantaana.com>
2023-09-14 15:45:30 -0500

    feat: :construction: send email

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
 
-const { config } = require('./../config/config');
+const AuthService = require('../services/auth.service');
 
+const service = new AuthService();
 const router = express.Router();
 
-router.post(
-  '/login',
-  passport.authenticate('local', { session: false }),
-  async (req, res, next) => {
+router
+  .post(
+    '/login',
+    passport.authenticate('local', { session: false }),
+    async (req, res, next) => {
+      try {
+        const user = req.user;
+
+        res.json(service.signToken(user));
+      } catch (error) {
+        next(error);
+      }
+    },
+  )
+
+  .post('/recovery', async (req, res, next) => {
     try {
-      const user = req.user;
-      const payload = {
-        sub: user.id,
-        role: user.role,
-      };
-      const token = jwt.sign(payload, config.jwtSecret);
-      res.json({
-        user,
-        token,
-      });
+      const { email } = req.body;
+      const result = 'hola' + email;
+      // await service.sendRecoveryPassword(email);
+
+      res.json(result);
     } catch (error) {
       next(error);
     }
-  },
-);
+  });
 
 module.exports = router;
